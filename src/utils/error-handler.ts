ccbc500e4f4c50ae71ffeeb2a90b48565ed42881
antosh <tomatomoving@mail.ru>
2025-06-03 15:04:45 +0300

    feat: added optional data in error api response

--- src/utils/error-handler.ts
+++ src/utils/error-handler.ts
@@ -4,11 +4,19 @@ import { sendResponse } from "./api-response";
 export function handleError(
   res: Response,
   error: Error,
-  statusCode: number = 500
+  statusCode: number = 500,
+  data?: unknown
 ) {
-  sendResponse(res, {
+  sendResponse<unknown>(res, {
     status: "error",
     code: statusCode,
     message: error.message || "Internal server error",
+    data,
   });
+  console.log(
+    `ERROR!
+     STATUS: ${statusCode}
+     ERROR OBJECT: ${error}
+     ERROR DATA: ${JSON.stringify(data)}`
+  );
 }
