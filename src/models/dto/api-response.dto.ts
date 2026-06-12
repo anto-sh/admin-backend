cfdb6eeab1efbbf3b7c59a20037e1ff7f1440a8a
antosh <tomatomoving@mail.ru>
2026-06-12 19:04:04 +0300

    refactor: removed useless props from ApiResponse type

--- src/models/dto/api-response.dto.ts
+++ src/models/dto/api-response.dto.ts
@@ -1,7 +1,5 @@
 export interface ApiResponse<T = any> {
   status: "success" | "error";
-  code: number; // HTTP статус-код
-  headers?: Record<string, string>; // Дополнительные заголовки
   message: string;
   data?: T; // Основные данные ответа
   timestamp?: string; // Дата формирования ответа
